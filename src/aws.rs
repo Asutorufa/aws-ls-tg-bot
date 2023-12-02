69dca580bf6b271d80d8d87d2c164679d9913eb9
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2023-12-02 10:33:37 +0800

    fix time range typo
    Signed-off-by: Asutorufa <16442314+Asutorufa@users.noreply.github.com>

--- src/aws.rs
+++ src/aws.rs
@@ -17,9 +17,52 @@ impl AwsClient {
             shared_config.region()
         );
 
+        // let cloudWatch = aws_sdk_cloudwatch::client::Client::new(&shared_config);
+
+        // let metrics_data_query_downloaded = aws_sdk_cloudwatch::types::MetricDataQuery::builder()
+        //     .set_id(Some("cloudfront_id".to_string()))
+        //     .set_metric_stat(Some(
+        //         aws_sdk_cloudwatch::types::MetricStat::builder()
+        //             .set_stat(Some("Sum".to_string()))
+        //             .set_period(Some(2700000))
+        //             .set_unit(Some(aws_sdk_cloudwatch::types::StandardUnit::Bytes))
+        //             .set_metric(Some(
+        //                 aws_sdk_cloudwatch::types::Metric::builder()
+        //                     .set_namespace(Some("AWS/CloudFront".to_string()))
+        //                     .set_metric_name(Some("BytesDownloaded".to_string()))
+        //                     .build(),
+        //             ))
+        //             .build(),
+        //     ))
+        //     .build();
+        // let metrics_data_query_uploaded = aws_sdk_cloudwatch::types::MetricDataQuery::builder()
+        //     .set_id(Some("cloudfront_id".to_string()))
+        //     .set_metric_stat(Some(
+        //         aws_sdk_cloudwatch::types::MetricStat::builder()
+        //             .set_stat(Some("Sum".to_string()))
+        //             .set_period(Some(2700000))
+        //             .set_unit(Some(aws_sdk_cloudwatch::types::StandardUnit::Bytes))
+        //             .set_metric(Some(
+        //                 aws_sdk_cloudwatch::types::Metric::builder()
+        //                     .set_namespace(Some("AWS/CloudFront".to_string()))
+        //                     .set_metric_name(Some("BytesDownloaded".to_string()))
+        //                     .build(),
+        //             ))
+        //             .build(),
+        //     ))
+        //     .build();
+
+        // let mrtrics = cloudWatch
+        //     .get_metric_data()
+        //     .set_metric_data_queries(Some(vec![{ metrics_data_query_downloaded }, {
+        //         metrics_data_query_uploaded
+        //     }]))
+        //     .send()
+        //     .await;
+
         return AwsClient {
             client: Client::new(&shared_config),
-            instance: instance,
+            instance,
         };
     }
 
@@ -33,14 +76,13 @@ impl AwsClient {
             Some(v) => v,
         };
 
-        let month;
-        if local.month() < 12 {
-            month = local.month() + 1;
+        let (month, year) = if local.month() < 12 {
+            (local.month() + 1, local.year())
         } else {
-            month = 1;
-        }
+            (1, local.year() + 1)
+        };
 
-        let end_date = match NaiveDate::from_ymd_opt(local.year(), month, 1) {
+        let end_date = match NaiveDate::from_ymd_opt(year, month, 1) {
             None => return Err("None EndDate".into()),
             Some(v) => v,
         };
@@ -80,6 +122,103 @@ mod test {
 
     use super::AwsClient;
     use chrono::{DateTime, Datelike, Local, NaiveDate, NaiveDateTime, NaiveTime};
+
+    #[tokio::test]
+    async fn get_cloudfront_data() {
+        let mut shared_config = aws_config::load_from_env().await;
+        shared_config = aws_config::SdkConfig::builder()
+            .http_connector((*shared_config.http_connector().unwrap()).clone())
+            .region(aws_types::region::Region::new("us-east-1"))
+            .credentials_cache((*shared_config.credentials_cache().unwrap()).clone())
+            .credentials_provider((*shared_config.credentials_provider().unwrap()).clone())
+            .build();
+        let cloud_watch = aws_sdk_cloudwatch::client::Client::new(&shared_config);
+
+        let metrics_data_query_downloaded = aws_sdk_cloudwatch::types::MetricDataQuery::builder()
+            .id("q1".to_string())
+            .return_data(true)
+            .metric_stat(
+                aws_sdk_cloudwatch::types::MetricStat::builder()
+                    .stat("Sum".to_string())
+                    .period(3600)
+                    .unit(aws_sdk_cloudwatch::types::StandardUnit::Bytes)
+                    .metric(
+                        aws_sdk_cloudwatch::types::Metric::builder()
+                            .dimensions(
+                                aws_sdk_cloudwatch::types::Dimension::builder()
+                                    .name("Region")
+                                    .value("Global")
+                                    .build(),
+                            )
+                            .dimensions(
+                                aws_sdk_cloudwatch::types::Dimension::builder()
+                                    .name("DistributionId")
+                                    .value("E1EYY8GARCLTWQ")
+                                    .build(),
+                            )
+                            .namespace("AWS/CloudFront".to_string())
+                            .metric_name("BytesDownloaded".to_string())
+                            .build(),
+                    )
+                    .build(),
+            )
+            .build();
+        let metrics_data_query_uploaded = aws_sdk_cloudwatch::types::MetricDataQuery::builder()
+            .id("cloudfront_id_upload".to_string())
+            .return_data(true)
+            .metric_stat(
+                aws_sdk_cloudwatch::types::MetricStat::builder()
+                    .stat("Sum".to_string())
+                    .period(3600)
+                    .unit(aws_sdk_cloudwatch::types::StandardUnit::Bytes)
+                    .metric(
+                        aws_sdk_cloudwatch::types::Metric::builder()
+                            .namespace("AWS/CloudFront".to_string())
+                            .metric_name("BytesDownloaded".to_string())
+                            .dimensions(
+                                aws_sdk_cloudwatch::types::Dimension::builder()
+                                    .name("Region")
+                                    .value("Global")
+                                    .build(),
+                            )
+                            .build(),
+                    )
+                    .build(),
+            )
+            .build();
+
+        let local: DateTime<Local> = Local::now();
+        let nd = match NaiveDate::from_ymd_opt(local.year(), local.month(), 1) {
+            None => panic!("none NaiveDate"),
+            Some(v) => v,
+        };
+
+        let month;
+        if local.month() < 12 {
+            month = local.month() + 1;
+        } else {
+            month = 1;
+        }
+
+        let end_date = match NaiveDate::from_ymd_opt(local.year(), month, 1) {
+            None => panic!("None EndDate"),
+            Some(v) => v,
+        };
+
+        let mrtrics = cloud_watch
+            .get_metric_data()
+            .start_time(aws_smithy_types::DateTime::from_secs(
+                NaiveDateTime::new(nd, NaiveTime::from_hms_micro_opt(0, 0, 0, 0).unwrap())
+                    .timestamp(),
+            ))
+            .end_time(aws_smithy_types::DateTime::from_secs(local.timestamp()))
+            .metric_data_queries(metrics_data_query_downloaded)
+            .metric_data_queries(metrics_data_query_uploaded)
+            .send()
+            .await;
+
+        println!("{:#?}", mrtrics.unwrap());
+    }
 
     #[tokio::test]
     async fn test_get_flow() {
